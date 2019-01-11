fc8809eaefe8d3183eb87125e7964da8be2aed35
chriscross00 <christopher.chan2014@gmail.com>
2019-01-10 23:56:51 -0800

    prepped work for tomorrow

--- list.py
+++ list.py
@@ -1,1 +1,6 @@
 #test
+#https://en.wikipedia.org/wiki/Dynamic_array#Geometric_expansion_and_amortized_cost
+#https://docs.python.org/3/tutorial/datastructures.html
+#https://www.geeksforgeeks.org/implementation-of-dynamic-array-in-python/
+
+print('hi')
