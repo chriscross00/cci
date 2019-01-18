50d6c905805f5ecf588e9f27c4e66221384cf7b7
chriscross00 <christopher.chan2014@gmail.com>
2019-01-17 23:41:59 -0800

    stop

--- ch1/ch1.2.py
+++ ch1/ch1.2.py
@@ -9,12 +9,16 @@ Approaches:
     - Time complexity: O(n!) Really bad
     - Space complexity: O(n!)
 
-
 2) Sort strings and check if order is the same
     - If the lengths aren't the same length return False
     - Sort the 2 strings
     - Compare the ith element in both strings
     - Time complexity: O(nLogn)
+
+3) Count the number of each character in each string
+    - For character in string store in dict
+    - Compare key, values in dict1 to dict2
+    - Time complexity: O(n)
 """
 
 
@@ -35,12 +39,44 @@ def perm(s1, s2):
 
 
 a = 'pop'
-b = 'cap'
+b = 'pop'
 
-print(perm(a, b))
+# print(perm(a, b))
+
+# This is a shorter version of above found on SO.
+
 
 def test(s1, s2):
     done = len(s1) == len(s2) and sorted(s1) == sorted(s2)
     return done
 
 
+# Counting
+
+def perm_count(s1, s2):
+
+    if len(s1) != len(s2):
+        return False
+
+    count1 = {}
+    count2 = {}
+
+    for i in s1:
+        if i in count1.keys():
+            count1[i] += 1
+        else:
+            count1[i] = 1
+    for i in s2:
+        if i in count2.keys():
+            count2[i] += 1
+        else:
+            count2[i] = 1
+
+    for i in count1.keys():
+        if count1[i] != count2[i]:
+            return False
+    else:
+        return True
+
+
+print(perm_count(a, b))
