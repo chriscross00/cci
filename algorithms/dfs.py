64aee91bf17fe30db1a3e3fd3a6aa3a19cc8e96f
Christopher Chan <christopher.chan2014@gmail.com>
2019-02-19 02:04:21 -0800

    add source of dfs

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -1,3 +1,5 @@
+# https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+
 graph = {'A': set(['B', 'C']),
          'B': set(['A', 'D', 'E']),
          'C': set(['A', 'F']),
@@ -68,4 +70,4 @@ def test(graph, start):
             stack.append(neighbor)
     return path
 
-print(test(adjacency_matrix, 1))
+print(test(graph, 'A'))
