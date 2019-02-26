5758622ec50072931a96096c06aa37dc345ee61a
chriscross00 <christopher.chan2014@gmail.com>
2019-02-26 00:45:38 -0800

    m

--- random_problems/matrix_traversal.py
+++ random_problems/matrix_traversal.py
@@ -1,6 +1,7 @@
 # https://snakify.org/en/lessons/two_dimensional_lists_arrays/
 # Time complexity: O(n^2)
 
+
 def mat_modifier(size):
     n = size
     matrix = [[0] * n for i in range(n)]
@@ -18,4 +19,20 @@ def mat_modifier(size):
         print(' '.join([str(j) for j in i]))
 
 
-mat_modifier(4)
+def better_mat(size):
+
+    n = size
+    matrix = [[0] * n for i in range(n)]
+
+    for i in range(n):
+        matrix[i][i] = 1
+        for j in range(0, i):
+            matrix[i][j] = 2
+        for j in range(i+1, n):
+            matrix[i][j] = 0
+
+    for i in matrix:
+        print(' '.join([str(j) for j in i]))
+
+
+better_mat(4)
