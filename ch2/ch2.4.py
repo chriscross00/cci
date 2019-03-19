78b120898dbcce0b10174bc349c0b862c5e39ecd
chriscross00 <christopher.chan2014@gmail.com>
2019-03-19 00:46:44 -0700

    created ch2.4

---
+++ ch2/ch2.4.py
@@ -0,0 +1,8 @@
+# Approach 1: Create 2 linked list, less and geq
+#
+# Iterate through original linked list and sort each node to appropriate
+# linked list
+# Append geq to less
+# Time: O(n)
+# Space: O(n)
+
