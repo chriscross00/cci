628433de9aa02364887e331a51f4813d5f51f2e2
chriscross00 <christopher.chan2014@gmail.com>
2019-06-10 23:58:12 -0700

    window practice

--- scratch.py
+++ scratch.py
@@ -1,155 +1,33 @@
-a = [87,42,32,52,90,22,74,25,90]
-b = [29, 5, 24, 9, 55, 83, 2, 0, 1, -1, 5, 90, 42]
+# Return min substring with all characters
+# A initialize 2 pointers
+# B increment right until all char encountered
+# C increment left until a char not in window, store score l-1 : r as best score
+# D repeat until right reaches end of string
 
-# dfs stack
-# bfs queue
+def window_solution(string, char_set):
+    left, right = 0, 0
+    best_score = float('inf')
+    best_substring = []
+    char_encountered = 0
+    letter_map = {}
 
+    while right < len(string):
+        letter_map[string[right]] = letter_map.get(string[right], 0) + 1
+        if letter_map[string[right]] == 1 and string[right] in char_set:
+            char_encountered += 1
+        right += 1
 
-def dfs(graph, start):
-
-    path = []
-    stack = [start]
-    levels = {start:0}
-    while stack:
-        vertex = stack.pop()
-
-        if vertex in path:
-            continue
-        path.append(vertex)
-        for nbr in graph[vertex]:
-            stack.append(nbr)
-            levels[nbr] = levels[vertex] + 1
-
-    print(levels)
-    return path
-
-
-def bfs2(graph, start):
-
-    path, queue = [], [start]
-
-    levels = {start:0}
-    while queue:
-        vertex = queue.pop(0)
-
-        if vertex not in path:
-            path.append(vertex)
-            queue.extend(graph[vertex])
-
-    return path
+        if char_encountered == len(char_set):
+            while char_encountered == len(char_set):
+                letter_map[string[left]] -= 1
+                if letter_map[string[left]] == 0 and string[left] in char_set:
+                    char_encountered -= 1
+                left += 1
+            best_score = min(best_score, right - left + 1)
+    return best_score
 
 
 
+print(window_solution("abdddddcbeba", {'a', 'b', 'c'}))
+print(window_solution("abweweffawefcaaaaboiwuroqiwuroiueeeb", {'a', 'b', 'c'}))
 
-def bfs(graph, start):
-
-    path, visited, queue = [], [],[start]
-    levels = {start:0}
-
-    while queue:
-        vertex = queue.pop(0)
-        path.append(vertex)
-
-        for nbr in graph[vertex]:
-            if nbr not in visited:
-                queue.append(nbr)
-                visited.append(nbr)
-
-                levels[nbr] = levels[vertex] + 1
-    print(levels)
-    return path
-
-graph = {}
-
-graph[0] = [1,5]
-graph[1] = [2]
-graph[2] = [3]
-graph[3] = [4,5]
-graph[4] = [0]
-graph[5] = [2,4]
-
-
-
-<<<<<<< HEAD
-def merge_sort:
-=======
-class Node:
->>>>>>> a02cde34b96f1c301af8db3c89f4bdf7b58f689f
-
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-    def set_data(self, data):
-        self.data = data
-
-    def get_data(self):
-        return self.data
-
-    def set_next(self, val):
-        self.next = val
-
-<<<<<<< HEAD
-=======
-    def get_next(self):
-        return self.next
-
-
-class LinkedList:
-
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
-
-    def add(self, data):
-        new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-        self.size += 1
-
-    def remove(self, val):
-        current = self.head
-        previous = None
-        found = False
-        while not found:
-            if current.get_data() == val:
-                found = True
-                self.size -= 1
-            else:
-                previous = current
-                current = current.get_next()
-        if previous is None:
-            self.head = current.get_next()
-        else:
-            previous.set_next(current.get_next())
-
-    def search(self, val):
-        current = self.head
-        found = False
-        while not found and current is not None:
-            if current.get_data() == val:
-                found = True
-            else:
-                current = current.get_next()
-
-        return found
-
-    def get_size(self):
-        return self.size
-
-ll = LinkedList()
-
-print(ll.get_size())
-
-ll.add(2)
-ll.add(3)
-ll.add(7)
-ll.add(5)
-ll.add(9)
-ll.add(8)
-
-
-
-
->>>>>>> a02cde34b96f1c301af8db3c89f4bdf7b58f689f
