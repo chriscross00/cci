29e3b3e257815635ea901d0a795783736a6f0403
chriscross00 <christopher.chan2014@gmail.com>
2019-06-23 01:02:11 -0700

    sigh

--- key_problems/new.py
+++ key_problems/new.py
@@ -3,11 +3,34 @@
 # 1. initialize stack
 # 2. iterate through string
 # 3. if char in open_bracket dict, push to stack
-# 4. if char is in closed_bracket dict, pop from
+# 4. if char is in closed_bracket dict, pop from stack
+# 5. compare to
 
 
 def solution(string):
 
-	for
+	stack = []
+	open_bracket = ['[', '{', '(']
+	closed_bracket = [']', '}', ')']
 
-	
+	for char in string:
+		if char in open_bracket:
+			stack.append(char)
+		elif char in closed_bracket:
+			if len(stack) == 0:
+				return False
+			temp = stack.pop()
+			if not bracket_match(temp, char):
+				return False
+
+	return len(stack) == 0
+
+
+def bracket_match(open, close):
+
+	mapper = {'{': '}', '[': ']', '(': ')'}
+	return mapper[open] == close
+
+
+print(solution("(3+9{12+4})(25)"))  # true
+print(solution("3+9{12+4})(25)"))  # false
